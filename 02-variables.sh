956fab1be28f4e1feb68b35f0e85e8302dd79c8f
joindevops90sb <lokam.nithisha@gmail.com>
2026-05-23 22:20:25 +0530

    added to user_name

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-echo " enter the user_name:"
+echo " enter the user_name : "
 
 read USER_NAME # this is variable when u enter  
 
 echo " user_name is $USER_NAME "
+
+echo " enter the password : "
+
+read -s password # this reads password with out visibility to others
+
+echo " the password is : "
+
