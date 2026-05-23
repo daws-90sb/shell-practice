22198c33b88be69345fbbbb0a19dcfd979806988
joindevops90sb <lokam.nithisha@gmail.com>
2026-05-23 22:25:07 +0530

    added to user_name

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-     echo " enter the user_name : "
+echo " enter the user_name : "
 
-read USER_NAME # this is variable when u enter  
+read   USER_NAME # this is variable when u enter  
 
 echo " user_name is $USER_NAME "
 
