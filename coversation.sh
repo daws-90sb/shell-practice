4379860abc819edb7005a4748d6da7fff0ce5c4b
joindevops90sb <lokam.nithisha@gmail.com>
2026-05-23 20:33:07 +0530

    added to coversion

--- coversation.sh
+++ coversation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-echo "Trump : Hey Iran we are going to blast you"
-echo "Iran : Hey Trump welcome"
-echo "Trump : I'am serious"
-echo "Iran : we are also serious"
+$PERSON1=RUSSIA
+$PERSON2=UKRAINE
+echo "$PERSON1 : Hey Iran we are going to blast you"
+echo "$PERSON2  : Hey Trump welcome"
+echo "$PERSON1 : Hey $PERSON2 I'am serious"
+echo "$PERSON2  : Hey $PERSON1 we are also serious"
