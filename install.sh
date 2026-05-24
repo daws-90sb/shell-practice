4ede9f5886ef79746b13304cf25efd3d65465af8
joindevops90sb <lokam.nithisha@gmail.com>
2026-05-24 21:43:27 +0530

    added special variables

--- install.sh
+++ install.sh
@@ -5,13 +5,23 @@ USERID=$(id -u)
 # check root access or not
 if [ $USERID -ne 0 ]; then 
    echo " please run the script with root access "
-   #exit 1
+   exit 1
 fi
 
 echo "I'm continuing...."
 
-#echo "Installing mysql"
-#dnf install mysql -y
+echo "Installing mysql"
+dnf install mysql -y
+
+
+if [ $? -ne 0 ]; then
+
+echo " Installing mysql ...... FAILED!"
+exit 1
+
+else
+
+echo " Installing mysql ...... SUCCEDED! "
 
 
  
