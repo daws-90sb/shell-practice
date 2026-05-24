49f898a5e2d057bcc866b68ad6ce2181409479db
joindevops90sb <lokam.nithisha@gmail.com>
2026-05-24 19:57:22 +0530

    added special variables

--- conditions.sh
+++ conditions.sh
@@ -10,7 +10,8 @@
 NUMBER=$1
 if [ $NUMBER -gt 10 ]; then
    echo " Given number $NUMBER is greater than 10 "
-
+elif [ $NUMBER -eq 10 ]; then
+   echo " given number $NUMBER is equal to 10 "   
 else 
    echo " Given number $NUMBER is less than 10 "
 
