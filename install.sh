b5b7f9c6466c5dc0cbcb7a01cfd387fb28bed194
joindevops90sb <lokam.nithisha@gmail.com>
2026-05-24 21:03:18 +0530

    added special variables

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ USERID=$(id -u)
 # check root access or not
 if [ $USERID -ne 0 ]; then 
    echo " please run the script with root access "
-   exit 1
-#fi
+   #exit 1
+fi
 
 echo "I'm continuing...."
 
