b91867ff9db158027d5ed51da9c4c047a3061c45
joindevops90sb <lokam.nithisha@gmail.com>
2026-05-26 10:28:46 +0530

    added special variables

--- 03-variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-TIME_STAMP=$(date)
-echo " TIMESTAMP is $TIME_STAMP "
-
-
--- coversation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PERSON1=RUSSIA
-PERSON2=UKRAINE
-
-echo "$PERSON1 : Hey Iran we are going to blast you"
-echo "$PERSON2  : Hey Trump welcome"
-echo "$PERSON1 : Hey $PERSON2 I'am serious"
-echo "$PERSON2  : Hey $PERSON1 we are also serious"
