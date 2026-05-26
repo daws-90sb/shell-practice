f00c0fc1b4a7c34d3cc8c81636edefb47bb85a42
joindevops90sb <lokam.nithisha@gmail.com>
2026-05-26 13:20:26 +0530

    API CREATED

--- 01-loops.sh
+++ 01-loops.sh
@@ -5,6 +5,7 @@
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script  #/home/ec2-user/shell-logs -> previous log files directory
 LOG_FILE=$LOGS_DIR/$0.log # /home/ec2-user/shell-logs/logs.sh.log -> previous log files irectory
+TIME_STAMP=$(date "+%y-%m-%d %H:%M:%S")
 
 # check root access or not
 if [ $USERID -ne 0 ]; then 
@@ -14,17 +15,17 @@ fi
 
 VALIDATE(){
     if [ $2 -ne 0 ]; then
-        echo " Installing $1 ...... FAILED!"
+        echo  " $TIME_STAMP [ERROR] " Installing $1 ...... FAILED!" | tee -a $LOG_FILE  
         exit 1
     else
-        echo " Installing $1 ...... SUCCEDED! "
+        echo "$TIME_STAMP [INFO] Installing $1 ...... SUCCEDED! " | tee -a $LOG_FILE
      fi
 }
 
 for package in $@
 do 
   echo " Installing ....$package "
-  dnf list installed $package
+  dnf list installed $package   &>> $LOG_FILE
   if [ $? -ne 0 ]; then
        dnf install $package -y  &>> $LOG_FILE
        VALIDATE  "Installing $package" $?
