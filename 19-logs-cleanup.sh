2a3ecb8ed6ab88ee0e9b1fbc37aa2133cde09c1e
daws-90sb <lokam.nithisha@gmail.com>
2026-06-02 03:56:17 +0530

    changes added

--- 19-logs-cleanup.sh
+++ 19-logs-cleanup.sh
@@ -3,7 +3,7 @@
 SOURCE_DIR=$1
 DAYS=${2:-14} # If $2 values is given then that values will be passed into days ex : $2= 5 is given means 5 days , else by default 14 days will be taken when no values is given into $2
 
-if [ $# -lt 1 ]; then
+if [ -z $SOURCE_DIR ]; then
     echo "ERROR :: Missing Parameters"
     echo "USAGE: $0 <SOURCE_DIR> [days(optional to default 14)]"
     exit 1
@@ -12,4 +12,22 @@ fi
 if [ ! -d $SOURCE_DIR ]; then
      echo "ERROR: Directory: $SOURCE_DIR does not exist"
      exit 1
-fi     
+fi
+
+echo "Scanning $SOURCE_DIR for log files older then 14 days"
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+if [ -z "$FILES" ]; then
+   echo "No log files older than 14 days found"
+   exit 0
+fi   
+
+while IFS=read -r FILE
+do
+    echo "FILE to be deleted : $FILE"
+done <<< $FILES
+
+# < ---> INPUT Symbols
+# > ---> OUTPUT Symbols
+# <  -> for file
+# <<<  -> variable (command output as input)
