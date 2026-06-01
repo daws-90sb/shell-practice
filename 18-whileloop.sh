5425638ce4056348465957414f445a92e89d5b69
daws-90sb <lokam.nithisha@gmail.com>
2026-06-02 01:58:25 +0530

    changes added

---
+++ 18-whileloop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+### while will take one one line into every single loop
+while IFS= read -r line # IFS Internal Field Separator
+do 
+
+echo $line
+
+done < 01-hello-world.sh # while loop will access this file and takes the each line into loop on every turn 
