eca8a8f23a3a87a98aa52b003324e5ab54f56b0d
daws-90sb <lokam.nithisha@gmail.com>
2026-06-04 01:03:54 +0530

    changes added

--- mail.sh
+++ mail.sh
@@ -8,7 +8,7 @@
     TO_ADDRESS=$5
     SUBJECT=$6
     
-    FINAL_MESSAGE=$(echo $MESSAGE | sed 's/[.[\*^$]/\\&/g')
+    FINAL_MESSAGE=$(echo $MESSAGE | sed 's/[.[\*^$]/\\&/g') #escape special characters in shell script using sed
     FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g"  -e "s/ALERT_TYPE/$ALERT_TYPE/g"  -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE/g" template.html)
 
 {
