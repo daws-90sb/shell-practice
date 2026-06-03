de9257a9e15d056beedf18aefb56fe2cdc765a21
daws-90sb <lokam.nithisha@gmail.com>
2026-06-03 21:42:22 +0530

    changes added

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -9,10 +9,10 @@ do
    USAGE=$( echo $line | awk '{print $6}' | cut -d "%" -f1)
    PARTITION=$( echo $line | awk '{print $7}')
    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-       MESSAGE+="HIGH DISK USAGE ON $PARTITION: $USAGE" # we can use + symbol in order to avoid the replav=cement of the message, if + is not used means then in evry loop the output lines will get replaced 
+       MESSAGE+="HIGH DISK USAGE ON $PARTITION: $USAGE \n" # we can use + symbol in order to avoid the replacement of the message, if + is not used means then in evry loop the output lines will get replaced 
        echo $line
    fi   
 
 done <<< "$DISK_USAGE"
  
- echo "$MESSAGE"
+ echo  -e "$MESSAGE" # -e is used to enable special characters
