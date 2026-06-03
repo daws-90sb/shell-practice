a9ded82107caccda9a0b971f206452ea7df31b76
daws-90sb <lokam.nithisha@gmail.com>
2026-06-03 21:37:25 +0530

    changes added

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -9,7 +9,7 @@ do
    USAGE=$( echo $line | awk '{print $6}' | cut -d "%" -f1)
    PARTITION=$( echo $line | awk '{print $7}')
    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-       MESSAGE="HIGH DISK USAGE ON $PARTITION: $USAGE"
+       MESSAGE+="HIGH DISK USAGE ON $PARTITION: $USAGE" # we can use + symbol in order to avoid the replav=cement of the message, if + is not used means then in evry loop the output lines will get replaced 
        echo $line
    fi   
 
