3857319ec0398bdb223c6e7b113c0054839d80d9
daws-90sb <lokam.nithisha@gmail.com>
2026-06-04 01:37:58 +0530

    changes added

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -2,7 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THRESHOLD=10
-SERVER_IP=&(curl -s http://169.254.169.254/latest/meta-data/local-ipv4) # this link is taken from google search => how i get server ip in aws ec2 curl command metadata
+SERVER_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4) # this link is taken from google search => how i get server ip in aws ec2 curl command metadata
 
 while IFS= read -r line
 do
@@ -11,7 +11,7 @@ do
    PARTITION=$( echo $line | awk '{print $7}')
    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
        MESSAGE+="HIGH DISK USAGE ON $PARTITION: $USAGE <br>" # we can use + symbol in order to avoid the replacement of the message, if + is not used means then in evry loop the output lines will get replaced 
-       echo $line
+       #echo $line
    fi   
 
 done <<< "$DISK_USAGE"
